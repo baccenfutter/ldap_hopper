652ca01a2609a78b92da65fd0638483034cefe51
Brian Wiborg <baccenfutter@c-base.org>
2022-03-13 23:21:00 +0100

    :arrow_up: Bumping deps

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        'python-ldap >= 3.3.1'
+        'python-ldap'
     ],
     extras_require={
         'dev': [
-            'build >= 0.6.0.post1',
-            'ipython >= 7.18.1',
-            'prompt-toolkit >= 3.0.8',
-            'pylint >= 2.6.0',
-            'twine >= 3.4.2',
+            'build',
+            'ipython',
+            'prompt-toolkit',
+            'pylint',
+            'twine',
         ]
     },
 )
